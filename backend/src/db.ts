eaa2f2b71798f89e61bb69c3d5703132dd745537
kajal-jotwani <kajaljotwani06@gmail.com>
2024-12-30 20:37:51 +0530

    created content schema

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -9,3 +9,12 @@ const UserSchema = new Schema({
 })
 
 export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+    tittle: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
+})
+
+export const Contentmodel = model("Content", ContentSchema);
