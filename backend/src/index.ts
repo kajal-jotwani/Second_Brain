29ffba48a649645667abc25d3477cd6f29518b7e
kajal-jotwani <kajaljotwani06@gmail.com>
2025-01-01 14:05:23 +0530

    created get and delete content routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -112,12 +112,26 @@ app.post("/api/v1/content",userMiddleware, async (req: Request, res: Response) =
     })
 })
 
-app.get("/api/v1/content", (req, res) => {
-    
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    const userId = req.userId;
+    const content = await Contentmodel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
 })
 
-app.delete("/api/v1/content", (req, res) => {
-    
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId
+
+    await Contentmodel.deleteMany({
+        contentId,
+        userId: req.userId
+    })
+    res.json({
+        message: "Deleted"
+    })
 })
 
 app.post("/api/v1/brain/share", (req, res) => {
